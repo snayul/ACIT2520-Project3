ef338bea7f976f5c9b25028b6484482ced33bea0
RaymondNguyn <raymond.nguyen37@gmail.com>
2024-04-10 00:08:24 -0700

    added in the controls for authentication, redirect to /reminder if pass back to /login if fails when logging in. As well as finished the register

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -1,4 +1,7 @@
 let database = require("../database");
+const passport = require("passport");
+
+
 
 let authController = {
   login: (req, res) => {
@@ -9,12 +12,31 @@ let authController = {
     res.render("auth/register");
   },
 
-  loginSubmit: (req, res) => {
-    // implement later
+  loginSubmit: (req, res, next) => {
+    passport.authenticate("local", {
+        successRedirect: "/reminders",
+        failureRedirect: "/login",
+        failureFlash: true
+    })(req, res, next);
   },
 
   registerSubmit: (req, res) => {
-    // implement later
+    try{
+      database.push({
+        id: database.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        reminders: []
+      })
+      console.log("Updated database:", database);
+
+      res.redirect('/login')
+    }
+    catch{
+      res.redirect('/register')
+    }
+    
   },
 };
 

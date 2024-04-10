47bf7c2c29764bf62d52a523e0be937e84508991
RaymondNguyn <raymond.nguyen37@gmail.com>
2024-04-10 00:11:35 -0700

    passport with local login

---
+++ middleware/passport.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+function initialize(passport, getUserByEmail, getUserById) {
+    console.log("Initializing Passport middleware");
+    const authenticateUser = async (email, password, done) => {
+        const user = getUserByEmail(email);
+        if (!user) {
+            console.log("No email", email)
+            return done(null, false, { message: "No user with that email" });
+        }
+
+        try {
+            if (password === user.password) {
+                console.log("User authenticated:", user);
+                return done(null, user);
+            } else {
+                console.log("Password incorrect for user:", user);
+                return done(null, false, { message: "Password incorrect" });
+            }
+        } catch (error) {
+            console.error("Error during authentication:", error);
+            return done(error);
+        }
+    };
+
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+    passport.serializeUser((user, done) => {
+        done(null, user.id)
+    });
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id));
+    });
+}
+
+module.exports = initialize;
